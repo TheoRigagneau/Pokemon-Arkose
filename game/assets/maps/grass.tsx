6d33b8cd613fca2904f064401d55b89d9724bc2b
TheoRigagneau <trigagneau@gmail.com>
2026-05-19 09:51:08 +0200

    add 2 house

---
+++ game/assets/maps/grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="grass" tilewidth="16" tileheight="16" tilecount="32" columns="16">
+ <image source="../tilesets/grass.png" width="256" height="32"/>
+</tileset>
---
+++ game/assets/maps/house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="house" tilewidth="16" tileheight="16" tilecount="19168" columns="16">
+ <image source="../tilesets/house.png" width="256" height="19168"/>
+</tileset>
