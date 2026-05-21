1e14d50aec02cf75b29859bee553d79649cf5ee3
TheoRigagneau <trigagneau@gmail.com>
2026-05-21 19:28:25 +0200

    starting storage for assets

---
+++ Assets_config.js
@@ -0,0 +1,14 @@
+import Assets from "./Assets.js";
+export const ASSETS = [
+    { key: "tileset", path: "./game/assets/tilesets/sprites.png" },
+    { key: "house", path: "./game/assets/tilesets/house.png" },
+    { key: "house2", path: "./game/assets/tilesets/house2.png" },
+    { key: "grass", path: "./game/assets/tilesets/grass.png" },
+    { key: "playerWalk", path: "./game/assets/tilesets/png/npc_198_Lucas.png" },
+    { key: "playerRun", path: "./game/assets/tilesets/png/npc_198_Lucas_run.png" },
+    { key: "npc_1", path: "./game/assets/tilesets/png/NPC_001_Ace_Trainer_M.png" },
+    { key: "npc_3", path: "./game/assets/tilesets/png/NPC_049_Collector.png" },
+    { key: "house", path: "./game/assets/tilesets/house.png" },
+    { key: "house", path: "./game/assets/tilesets/house.png" },
+    { key: "house", path: "./game/assets/tilesets/house.png" },
+     ]
